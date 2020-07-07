2b6899f82d92127d4cba5eac3e27f1c4e165449b
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2020-07-07 14:36:57 +0200

    Upload daily RIVM data 07-07
    Former-commit-id: dcd77d3c74172462625bc3f78578734d9a276ba6

--- daily_updates_RIVM.R
+++ daily_updates_RIVM.R
@@ -1,4 +1,3 @@
-require(taskscheduleR)
 require(dplyr)
 require(rjson)
 require(rtweet)
@@ -39,9 +38,9 @@ cases.yesterday <- head(diff(rivm.daily_aggregate$cases),n=1)*-1 ## Calculate ne
 hospital.yesterday <- head(diff(rivm.daily_aggregate$hospitalization),n=1)*-1 ## Calculate new hospitalizations
 deaths.yesterday <- head(diff(rivm.daily_aggregate$deaths),n=1)*-1 ## Calculate new deaths
 
-cases.patient <- ifelse(cases.yesterday == 1, "patient","patienten")
-hospital.patient <- ifelse(hospital.yesterday == 1, "patient","patienten")
-deaths.patient <- ifelse(deaths.yesterday == 1, "patient","patienten")
+cases.patient <- ifelse(cases.yesterday == 1, "patiënt","patiënten")
+hospital.patient <- ifelse(hospital.yesterday == 1, "patiënt","patiënten")
+deaths.patient <- ifelse(deaths.yesterday == 1, "patiënt","patiënten")
 
 
 ## Build tweets
@@ -61,16 +60,14 @@ tweet
 post_tweet(status = tweet) ## Post tweet
 
 my_timeline <- get_timeline(rtweet:::home_user()) ## Pull my own tweets
-reply_id <- my_timeline$status_id[4] ## Status ID for reply
+reply_id <- my_timeline$status_id[1] ## Status ID for reply
 post_tweet("Voor een veel uitgebreidere update verwijs ik graag naar de dagelijkse updates van @edwinveldhuizen die dit ook per gemeente doet.",
            in_reply_to_status_id = reply_id) ## Post reply
 
 
-post_tweet(status = "Het aantal positief geteste per provincie sinds 1 juni. Zuid-Holland stijgt nog steeds door terwijl de rest afvlakt/afgevlakt is.",media="C:/Users/s379011/surfdrive/projects/CoronaWatchNL/plots/province_count_time.png",in_reply_to_status_id = reply_id)
+post_tweet(status = "Het aantal positieve tests, ziekenhuisopnames, en overledenen per geslacht sinds 1 juni. Iets meer vrouwen dan mannen die positief getest zijn.",media="C:/Users/s379011/surfdrive/projects/CoronaWatchNL/plots/toename_plot_geslacht.png",in_reply_to_status_id = reply_id)
 
-post_tweet(status ="Laatste voor vandaag: het aantal besmettingen per leeftijdsgroep vanaf 1 juni. Meeste besmettingen in groep 0-59 en laatste drie weken minimaal aantal in groep 60+.",media="C:/Users/s379011/surfdrive/projects/CoronaWatchNL/plots/overview_plot_leeftijd.png",
-           in_reply_to_status_id = reply_id)
-
+post_tweet(status = "Het RIVM publiceert nu de wekelijkse updates op dinsdag (vandaag dus). Zie voor de update over afgelopen week de site van het @RIVM: https://www.rivm.nl/coronavirus-covid-19/actueel",in_reply_to_status_id = reply_id)
 
 ## Data for municipalities
 
