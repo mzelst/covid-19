cbe0042796c23de1209c9b7533d241abcbae44fa
mzelst <j.m.vanzelst@tilburguniversity.edu>
2020-07-21 14:28:35 +0200

    Daily upload RIVM and NICE data 21-07

--- daily_updates_RIVM.R
+++ daily_updates_RIVM.R
@@ -216,7 +216,7 @@ Opgenomen: ",last(all.data$new.hospitals),"
 Totaal: ",last(all.data$hospitalization),ifelse(last(all.data$net.hospitals)>=0," (+"," (-"),abs(last(all.data$net.hospitals))," ivm ",last(all.data$corrections.hospitals)," corr.)
 
 Opgenomen op IC: ",last(all.data$IC_Intake_Proven),"
-Totaal: ",tail(all.data$IC_Cumulative,n=1)," 
+Totaal: ",tail(all.data$IC_Cumulative,n=1)," (-1 ivm. 1 corr.)
 
 Overleden: ",last(all.data$new.deaths),"
 Totaal: ",last(all.data$deaths),ifelse(last(all.data$net.deaths)>=0," (+"," (-"),abs(last(all.data$net.deaths))," ivm ",last(all.data$corrections.deaths)," corr.)")
@@ -248,10 +248,15 @@ post_tweet(tweet2, media = "plots/plot_daily.png",
 
 my_timeline <- get_timeline(rtweet:::home_user()) ## Pull my own tweets
 reply_id <- my_timeline$status_id[1] ## Status ID for reply
+post_tweet(status = "Bevestiging van wat ik afgelopen week dus al 'voorspelde': het aantal tests was 81269 (8.5% meer dan vorige week) met een netto toename van 817 besmettingen (76.4% meer dan vorige week). De pos. rate stijgt dus van 0.6% naar 1.0% en komt dus vooral door daadwerkelijke toename.", 
+           in_reply_to_status_id = reply_id) ## Post reply
+
+my_timeline <- get_timeline(rtweet:::home_user()) ## Pull my own tweets
+reply_id <- my_timeline$status_id[1] ## Status ID for reply
 post_tweet("Vergeet ook niet de tweets hieronder van @edwinveldhuizen te checken voor de regionale verschillen en trends. Wellicht zijn deze nog belangrijker dan de landelijke cijfers.",
            in_reply_to_status_id = reply_id) ## Post reply
 
 
 my_timeline <- get_timeline(rtweet:::home_user()) ## Pull my own tweets
 reply_id <- my_timeline$status_id[1] ## Status ID for reply
-post_tweet(status = "Het RIVM publiceert nu de wekelijkse updates op dinsdag (vandaag dus). Zie voor de update over afgelopen week de site van het @RIVM: https://www.rivm.nl/sites/default/files/2020-07/COVID-19_WebSite_rapport_wekelijks_20200714_1040.pdf",in_reply_to_status_id = reply_id)
+post_tweet(status = "Het RIVM publiceert nu de wekelijkse updates op dinsdag (vandaag dus). Zie voor de update over afgelopen week de site van het @RIVM: https://www.rivm.nl/sites/default/files/2020-07/COVID-19_WebSite_rapport_wekelijks_20200721_1135.pdf",in_reply_to_status_id = reply_id)
