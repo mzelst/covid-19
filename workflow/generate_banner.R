7f31f534f243e00df0eaacfd4e5a6523dc25cc6a
Edwin Veldhuizen <info@edwinveldhuizen.com>
2020-08-22 09:10:53 +0200

    [ Banners ] generate with R
    Former-commit-id: 00de08acfad13bd236df572a9b91b1373f4021da

---
+++ workflow/generate_banner.R
@@ -0,0 +1,15 @@
+require(magick);
+require(stringr);
+
+Sys.setlocale("LC_TIME", "nl_NL")
+date <- Sys.Date() %>%
+  format('%d %B %Y') %>%
+  str_to_title()  %>%
+  str_replace( '^0', '')
+
+banner <- image_read('banners/template.png') %>%
+  image_annotate( date, font = 'Cabin SemiBold', size = 40,  location = "+0+80", gravity = "center")
+
+image_write(banner, str_c("banners/",Sys.Date(),".png") )
+
+rm(list=ls()) # Clean environment
