a2f27c3b3d6bbffe576e97162f0e86bd1c23c7b3
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2020-08-29 14:17:18 +0200

    Daily (automated) update RIVM and NICE data 2020-08-29
    Former-commit-id: 09718792ce934d7b98c09fcb43fbe7fdaee1f5de

--- workflow/daily_update.R
+++ workflow/daily_update.R
@@ -37,7 +37,11 @@ rm(list=ls()) # Clean environment
 all.data <- read.csv("data/all_data.csv")
 nice_by_day <- read.csv("data/nice_by_day.csv")
 
+git.credentials <- read_lines("git_auth.txt")
+git.auth <- cred_user_pass(git.credentials[1],git.credentials[2])
+
 ## Push to git
+repo <- init()
 add(repo, path = "*")
 commit(repo, all = T, paste0("Daily (automated) update RIVM and NICE data ",Sys.Date()))
 push(repo, credentials = git.auth)
