04fdb1ad1fe591be9bc36ea7d8f7e8b3c6943ff1
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2020-08-30 10:45:11 +0200

    Restructure date_statistics mutations file
    Former-commit-id: 64636e7c06b0298ff024ef941bfd515a5294828a

--- workflow/dashboards/date_statistics_mutations.R
+++ workflow/dashboards/date_statistics_mutations.R
@@ -32,7 +32,7 @@ date_type_wide <- date_type_wide[,c("DON","DOO","DPL","Datum")]
 
 dat_wide <- merge(date_type_wide, df.final, by = "Datum")
 
-write.csv(dat_wide, file = "date_statistics_mutations.csv")
+write.csv(dat_wide, file = "data-dashboards/date_statistics_mutations.csv")
 add(repo, path = "data-dashboards/date_statistics_mutations.csv")
 commit(repo, all = T, paste0("Update mutations per date_statistics types ",Sys.Date()))
 push(repo, credentials = git.auth)
