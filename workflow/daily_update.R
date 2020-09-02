bf178489edacc52a4da9672ee51d04336ba94b90
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2020-09-02 15:30:31 +0200

    Upload new geographical maps with most recent data
    Former-commit-id: 58e55d8143d2374ed584645c9b9a3befcefab1c2

--- workflow/daily_update.R
+++ workflow/daily_update.R
@@ -26,7 +26,7 @@ all.data <- all.data[order(all.data$date),]
 write.csv(all.data, file = "data/all_data.csv",row.names = F)
 
 source("plot_scripts/daily_plots.R")
-#source("plot_scripts/daily_maps_plots.R")
+source("plot_scripts/daily_maps_plots.R")
 
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc"); rmarkdown::render('reports/daily_report.Rmd') ## Render daily report
 file.copy(from = list.files('reports', pattern="*.pdf",full.names = TRUE), 
