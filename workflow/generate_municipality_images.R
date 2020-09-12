f6792078a6b21432f328f4dd30b999bee8dadecd
Edwin Veldhuizen <info@edwinveldhuizen.com>
2020-09-12 14:10:09 +0200

    [ daily_municipality ] Setup few test scripts

--- workflow/generate_municipality_images.R
+++ workflow/generate_municipality_images.R
@@ -1,3 +1,9 @@
 require(webshot2)
+require(rmarkdown)
 
-rmdshot("workflow/daily_municipality.Rmd", "plots/list_municipality.png", delay = 1)
+rmarkdown::render(
+  input = "workflow/daily_municipality_test.Rmd", 
+  #output_file = "reports/daily_municipality/main.html",
+  output_file = "main.html",
+  output_format = "all")
+#rmdshot("workflow/daily_municipality_test.Rmd", "plots/list_municipality.png", delay = 60, webshot_timeout = 120)
