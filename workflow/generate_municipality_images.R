fa7d6b454a3d35f679cbd94ea44264326e85e2bd
Edwin Veldhuizen <info@edwinveldhuizen.com>
2020-09-14 08:53:09 +0200

    [ generate-image ] make a good start
    Former-commit-id: da4a75b6bde7162f8752d78680f1749acb277af7

--- workflow/generate_municipality_images.R
+++ workflow/generate_municipality_images.R
@@ -1,9 +1,17 @@
-require(webshot2)
-require(rmarkdown)
+require(webshot2);
+require(rmarkdown);
+require(magick);
 
 rmarkdown::render(
-  input = "workflow/daily_municipality_test.Rmd", 
+  input = "workflow/daily_municipality.Rmd", 
   #output_file = "reports/daily_municipality/main.html",
   output_file = "main.html",
   output_format = "all")
-#rmdshot("workflow/daily_municipality_test.Rmd", "plots/list_municipality.png", delay = 60, webshot_timeout = 120)
+
+webshot(
+  paste("file://", getwd(), "/workflow/main.html", sep=''),
+  paste(getwd(), "/plots/list_municipality.png", sep=''),
+  zoom = 2
+)
+
+# rmdshot("workflow/daily_municipality.Rmd", "plots/list_municipality.png", delay = 5, webshot_timeout = 120)
