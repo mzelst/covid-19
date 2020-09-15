de010a363d60a42d87659f3019e9127781024eed
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2020-09-15 14:19:47 +0200

    Daily (automated) update RIVM and NICE data 2020-09-15

--- workflow/parse_nice-data.R
+++ workflow/parse_nice-data.R
@@ -58,10 +58,14 @@ zkh_new$new_hosp_proven <- unlist(zkh_new$V2)
 zkh_new$new_hosp_suspected <- unlist(zkh_new$V3)
 zkh_new <- zkh_new[,c(4:6)]
 
+hospital.cumulative <- rjson::fromJSON(file = "https://www.stichting-nice.nl/covid-19/public/zkh/intake-cumulative/",simplify = TRUE) %>%
+  map(as.data.table) %>%
+  rbindlist(fill = TRUE)
+
 
 # Merge all data
-df <- data.frame(zkh_new,ic_intake,ic_current$value,ics.used$value,ic.cumulative$value,zkh_current$value,ic.death_survive)
-names(df) <- c("date","Hospital_Intake_Proven","Hospital_Intake_Suspected","IC_Intake_Proven","IC_Intake_Suspected","IC_Current","ICs_Used","IC_Cumulative","Hospital_Currently","IC_Deaths_Cumulative","IC_Discharge_Cumulative","IC_Discharge_InHospital")
+df <- data.frame(zkh_new,ic_intake,ic_current$value,ics.used$value,ic.cumulative$value,zkh_current$value,ic.death_survive, hospital.cumulative$value)
+names(df) <- c("date","Hospital_Intake_Proven","Hospital_Intake_Suspected","IC_Intake_Proven","IC_Intake_Suspected","IC_Current","ICs_Used","IC_Cumulative","Hospital_Currently","IC_Deaths_Cumulative","IC_Discharge_Cumulative","IC_Discharge_InHospital","Hospital_Cumulative")
 df$Hospital_Intake <- df$Hospital_Intake_Proven + df$Hospital_Intake_Suspected
 df$IC_Intake <- df$IC_Intake_Proven + df$IC_Intake_Suspected
 
