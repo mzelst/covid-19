85983d5546c8e4b0cdba484f19c47b6aaddbe878
Edwin Veldhuizen <info@edwinveldhuizen.com>
2020-09-21 12:47:35 +0200

    [ municipalities ] auto-generate all images
    Former-commit-id: d3ce7bc8f35aae22df3ed92465e2ea6f15da11c3

--- workflow/generate_municipality_images.R
+++ workflow/generate_municipality_images.R
@@ -1,17 +1,47 @@
-require(webshot2);
-require(rmarkdown);
 require(magick);
+require(rmarkdown);
+require(webshot2);
 
 rmarkdown::render(
   input = "workflow/daily_municipality.Rmd", 
   #output_file = "reports/daily_municipality/main.html",
-  output_file = "main.html",
+  output_file = "daily_municipality.html",
   output_format = "all")
 
-webshot2::webshot(
-  paste("file://", getwd(), "/workflow/main.html", sep=''),
-  paste(getwd(), "/plots/list_municipality.png", sep=''),
-  zoom = 2
+screenshots = c(
+  "section" = "full",
+  "section.cases header" = "cases-top",
+  "section.cases main" = "cases-all",
+  "section.hosp header" = "hosp-top",
+  "section.hosp main" = "hosp-all",
+  "section.deaths header" = "deaths-top",
+  "section.deaths main" = "deaths-all"
 )
 
-# rmdshot("workflow/daily_municipality.Rmd", "plots/list_municipality.png", delay = 5, webshot_timeout = 120)
+for (i in seq_along(screenshots)) {
+  selector <- names(screenshots)[i]
+  file <- paste(getwd(), '/plots/list-', screenshots[i], '.png', sep='')
+  webshot2::webshot(
+    url = paste("file://", getwd(), "/workflow/daily_municipality.html", sep=''),
+    file = file,
+    zoom = 2,
+    selector = selector, 
+    expand = c(5, 5, 5, 5)
+  )
+  
+  if (grepl("main", selector, fixed=TRUE)) {
+    image.list <- image_read(file)
+    image.list.width <- image_info(image.list)["width"]
+    image.list.height <- image_info(image.list)["height"]
+    image_crop(image.list, geometry_area(image.list.width, 2830, 0, 0)) %>%
+      image_write(paste(getwd(), '/plots/list-', screenshots[i], '-part1.png', sep=''))
+    
+    image_crop(image.list, geometry_area(image.list.width, 3020, 0, 2840)) %>%
+      image_write(paste(getwd(), '/plots/list-', screenshots[i], '-part2.png', sep=''))
+    
+    image_crop(image.list, geometry_area(image.list.width, image.list.height - 5875, 0, 5875)) %>%
+      image_write(paste(getwd(), '/plots/list-', screenshots[i], '-part3.png', sep=''))
+  }
+}
+
+rm(list=ls())
