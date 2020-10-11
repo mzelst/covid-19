184579b9dd3ca082e5ab7ab89491ee05cf8497fc
Edwin Veldhuizen <info@edwinveldhuizen.com>
2020-10-11 13:55:12 +0200

    [ tweet_municipality ] Finalize script
    Former-commit-id: 2ede016c4c1b0018089379061b066d550117d82e

--- workflow/tweet_municipality.R
+++ workflow/tweet_municipality.R
@@ -20,28 +20,30 @@ format_custom_number <- function(data, plus = FALSE, format = "%s") {
 
 tweet_detailed <- function(data){
   counter <<- counter + 1
-  cat(paste('tweet', counter, '/ 355 :', data$municipality, "\n"))
+  cat(paste('tweet', counter, '/ 300 :', data$municipality, "\n"))
   
   more_or_less <- (data$d0 - data$d7) / (data$d7 - data$d14)
-  tweet <- sprintf("%s %s %s %s ( w: %s ) %s
-  
+  tweet <- sprintf("%s %s %s
+
 %s sinds gisteren
-%s sinds 1 september
-%s sinds 7 dagen ( %s )
-Wat %s is dan de %s in de 7 dagen ervoor
-%s inwoners maakt dat %s %.1f / 100.000 / 7d
-                   
+%s sinds 1 sep
+%s sinds 7 dagen (%s)
+Wat %s is dan de %s in de 7d ervoor
+%s inwoners maakt dat
+%s %.1f / 100.000 / 7d
+
+Daarbij in de laatste 2 weken:
+Opnames: %s
+Overlijdens: %s
+
 [#COVID19NL %s]",
-    format_custom_number(data$increase_1d, TRUE),
     data$color, 
     data$municipality,
-    format_custom_number(data$current, FALSE),
-    format_custom_number(data$increase_7d, TRUE),
     data$growth,
     ifelse(data$increase_1d == 0, 0, format_custom_number(data$increase_1d, TRUE)),
     format_custom_number(data$current, TRUE),
     format_custom_number(data$increase_7d, TRUE),
-    format(used_date - 7, '%d-%m-%Y'), 
+    tweet.date7d, 
     ifelse(    more_or_less >= 2, "fors meer",
        ifelse( more_or_less >= 1, "meer",
        ifelse( more_or_less == 1, "evenveel",  
@@ -51,10 +53,13 @@ Wat %s is dan de %s in de 7 dagen ervoor
     format_custom_number(data$d7 - data$d14, TRUE),
     format_custom_number(data$population),
     data$color, 
-    data$rel_increase_7d, 
+    data$rel_increase_7d,
+    format_custom_number(dat.hosp[dat.hosp$Municipality_code==data$Municipality_code, "increase_14d"], TRUE),
+    format_custom_number(dat.deaths[dat.deaths$Municipality_code==data$Municipality_code, "increase_14d"], TRUE),
     tweet.date
   )
   Encoding(tweet) <- "UTF-8"
+  cat(paste(tweet, "\n"))
   posted_tweet <<- post_tweet(tweet, 
     in_reply_to_status_id = reply_id, ## Post reply
     token = token.spamedwin,
@@ -64,9 +69,14 @@ Wat %s is dan de %s in de 7 dagen ervoor
   reply_id <<- posted_tweet$id_str
 }
 
+dat.deaths <- read.csv("data/municipality-deaths-today-detailed.csv", fileEncoding = "UTF-8")
+dat.hosp <- read.csv("data/municipality-hospitalisations-today-detailed.csv", fileEncoding = "UTF-8")
 dat.cases <- read.csv("data/municipality-today-detailed.csv", fileEncoding = "UTF-8") %>%
   filter(Municipality_code != "") %>%
-  arrange(desc(population)) %>%
+  arrange(
+    match(Municipality_code, c("GM0088", "GM0096", "GM0060", "GM0093", "GM0448", "GM0180", "GM0744", "GM0244", "GM0946")),
+    desc(population)
+  ) %>%
   head(300) %>%
   arrange(municipality)
 
@@ -74,6 +84,10 @@ used_date <- as.Date(last(dat.cases$date))
 tweet.date <- used_date %>%
   format('%d %b') %>%
   str_to_title()  %>%
+  str_replace( '^0', '')
+
+tweet.date7d <- (used_date - 7) %>%
+  format('%d %b') %>%
   str_replace( '^0', '')
 
 tweet <- sprintf("Gedetailleerd overzicht van alle gemeentes %s
