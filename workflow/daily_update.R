fc92c67888b7d65dc062489bfbfa35e4e2a160aa
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2020-10-16 14:19:27 +0200

    Daily (automated) update RIVM and NICE data 2020-10-16
    Former-commit-id: dff11525861babfcd0490f3c253560971e7b748f

--- workflow/daily_update.R
+++ workflow/daily_update.R
@@ -49,8 +49,8 @@ text.deaths.corrections <- paste0(ifelse(last(all.data$net.deaths)>=0," (+"," (-
 ## Build tweets
 tweet <- paste0("#COVID19NL statistieken t.o.v. gisteren: 
 
-Positief getest: ",7833,"
-Totaal: ",last(all.data$cases)," (+",last(all.data$net.infection)," ivm ",-42," corr.)
+Positief getest: ",7997,"
+Totaal: ",last(all.data$cases)," (+",last(all.data$net.infection)," ivm ",-13," corr.)
 
 Opgenomen: ",last(all.data$new.hospitals),"
 Totaal: ",last(all.data$hospitalization),ifelse(last(all.data$corrections.hospitals)<0,text.hosp.corrections,""),"
