2b38252f4aa3fdd25bd2f78ee6898ee596fe2088
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2020-11-04 14:23:27 +0100

    Daily (automated) update RIVM and NICE data 2020-11-04
    Former-commit-id: 5016b8ed6a7a8ff20d3ced83dec139d1c9911602

--- workflow/parse_rivm-data.R
+++ workflow/parse_rivm-data.R
@@ -7,7 +7,7 @@ rm(list=ls())
 
 # Data municipalities per day
 rivm.mun.perday <- read.csv("https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_per_dag.csv", sep=";")
-sum(rivm.mun.perday$Total_reported)
+sum(rivm.mun.perday$Total_reported)-375890
 last_date <- as.Date(last(rivm.mun.perday$Date_of_report))
 filename.mun.perday <- paste0("data-rivm/municipal-datasets-per-day/rivm_municipality_perday_", last_date, ".csv") ## Filename for daily data municipalities
 write.csv(rivm.mun.perday, file=filename.mun.perday,row.names = F)
