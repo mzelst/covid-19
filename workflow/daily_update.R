88a096ad1cdb9dde6276092a6b78972ed273dec6
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2020-11-07 14:22:36 +0100

    Daily (automated) update RIVM and NICE data 2020-11-07
    Former-commit-id: 67993d5cea55c4a81f7960cb70cda58ad11d98c2

--- workflow/daily_update.R
+++ workflow/daily_update.R
@@ -5,8 +5,8 @@ source("workflow/generate_banner.R")
 
 # Parse RIVM, NICE and corrections data
 source("workflow/parse_nice-data.R")
-source("workflow/parse_rivm-data.R")
 source("workflow/parse_lcps-data.R")
+source("workflow/parse_rivm-data.R")
 source("workflow/parse_municipalities.R")
 source("workflow/parse_corrections.R")
 
