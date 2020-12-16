d20bab5d3db1c4ffef0601ff38d6614122855be8
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2020-12-16 09:06:58 +0100

    Add NICE temporary checker
    Former-commit-id: 9d79f0c121ebb140e9fd02612c46b1737a25c08b [formerly 6a6390b4c0adb3c078f0e51ee5ed10c4ccce4ae0]
    Former-commit-id: fb92c97ae3ce9ad7dc17ef94593389d7173bea66

---
+++ misc/scripts/NICE checker.R
@@ -0,0 +1,22 @@
+#### NICE CHECKER ####
+
+source("workflow/parse_nice-data.R")
+
+temp = tail(list.files(path = "data-nice/data-nice-json/",pattern="*.csv", full.names = T),4)
+myfiles = lapply(temp, read.csv)
+
+dat.today <- as.data.frame(myfiles[4])
+dat.yesterday <- as.data.frame(myfiles[3])
+dat.twodaysago <- as.data.frame(myfiles[2])
+dat.threedaysago <- as.data.frame(myfiles[1])
+
+sum(dat.today$Hospital_Intake_Proven) - sum(dat.yesterday$Hospital_Intake_Proven)
+
+sum(dat.today$IC_Intake_Proven) - sum(dat.yesterday$IC_Intake_Proven)
+
+
+
+df <- merge(dat.today[,c("date","Hospital_Intake_Proven","Hospital_Intake_Suspected","IC_Intake_Proven")], dat.yesterday[,c("date","Hospital_Intake_Proven","Hospital_Intake_Suspected","IC_Intake_Proven")], by = "date", all.x=T)
+df$diff.proven <- df$Hospital_Intake_Proven.x-df$Hospital_Intake_Proven.y
+df$diff.suspec <- df$Hospital_Intake_Suspected.x-df$Hospital_Intake_Suspected.y
+df$diff.proven.ic <- df$IC_Intake_Proven.x-df$IC_Intake_Proven.y
