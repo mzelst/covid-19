6c40b5dd7c3f007957d4a5c61f49e29a734cac14
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2021-01-09 15:18:01 +0100

    [2021-01-09] Daily (automated) update RIVM and NICE data part 1/2
    Former-commit-id: 2640edbf1a944ac03bfc382c979d3587e88a0758

--- workflow/parse_rivm-data.R
+++ workflow/parse_rivm-data.R
@@ -16,8 +16,8 @@ condition <- Sys.Date()!=as.Date(last(rivm.mun.perday$Date_of_report))
 #} else {
 
 # Parse data municipality per day 
-sum(rivm.mun.perday$Total_reported)-850790
-sum(rivm.mun.perday$Deceased)-12084
+sum(rivm.mun.perday$Total_reported)-858914
+sum(rivm.mun.perday$Deceased)-12171
 last_date <- as.Date(last(rivm.mun.perday$Date_of_report))
 filename.mun.perday <- paste0("data-rivm/municipal-datasets-per-day/rivm_municipality_perday_", last_date, ".csv") ## Filename for daily data municipalities
 write.csv(rivm.mun.perday, file=filename.mun.perday,row.names = F)
