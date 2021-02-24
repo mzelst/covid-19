df9483b2798b59bc0d71db50a3d8725d79742af5
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2021-02-24 15:04:35 +0100

    Reformat NICE age total table

--- workflow/dashboards/age-distribution-date-NICE.R
+++ workflow/dashboards/age-distribution-date-NICE.R
@@ -7,15 +7,15 @@ nice_by_day_clinical <- map_dfr(myfiles, ~{ ## Write dataframe of all day files
   .x
 })
 
-nice_by_day_clinical <- nice_by_day_clinical[,c("Leeftijd","Klinisch_aanwezig","Datum")]
+nice_by_day_clinical_aanwezig <- nice_by_day_clinical[,c("Leeftijd","Klinisch_aanwezig","Datum")]
 
-perc <- nice_by_day_clinical %>% 
+perc <- nice_by_day_clinical_aanwezig %>% 
   group_by(Datum) %>% mutate(value = round((Klinisch_aanwezig/sum(Klinisch_aanwezig))*100,2))
 
-nice_by_day_perc <- cbind(nice_by_day_clinical[,c("Leeftijd","Datum")],as.numeric(perc$value))
+nice_by_day_perc <- cbind(nice_by_day_clinical_aanwezig[,c("Leeftijd","Datum")],as.numeric(perc$value))
 colnames(nice_by_day_perc) <- c("Leeftijd","Datum","Klinisch_aanwezig_Percentage")
 
-Klinisch_aanwezig <- nice_by_day_clinical %>%
+Klinisch_aanwezig <- nice_by_day_clinical_aanwezig %>%
   spread(Datum,value = Klinisch_aanwezig)
 
 Klinisch_aanwezig_Percentage <- nice_by_day_perc %>%
@@ -33,15 +33,15 @@ nice_by_day_IC <- map_dfr(myfiles, ~{ ## Write dataframe of all day files
   .x
 })
 
-nice_by_day_IC <- nice_by_day_IC[,c("Leeftijd","IC_aanwezig","Datum")]
+nice_by_day_IC_aanwezig <- nice_by_day_IC[,c("Leeftijd","IC_aanwezig","Datum")]
 
-perc <- nice_by_day_IC %>% 
+perc <- nice_by_day_IC_aanwezig %>% 
   group_by(Datum) %>% mutate(value = round((IC_aanwezig/sum(IC_aanwezig))*100,2))
 
-nice_by_day_perc <- cbind(nice_by_day_IC[,c("Leeftijd","Datum")],as.numeric(perc$value))
+nice_by_day_perc <- cbind(nice_by_day_IC_aanwezig[,c("Leeftijd","Datum")],as.numeric(perc$value))
 colnames(nice_by_day_perc) <- c("Leeftijd","Datum","IC_Aanwezig_Percentage")
 
-IC_Aanwezig <- nice_by_day_IC %>%
+IC_Aanwezig <- nice_by_day_IC_aanwezig %>%
   spread(Datum,value = IC_aanwezig)
 
 IC_Aanwezig_Percentage <- nice_by_day_perc %>%
@@ -52,14 +52,19 @@ write.csv(IC_Aanwezig_Percentage, file = "data-nice/age/leeftijdsverdeling_datum
 
 # Merge clinical and IC data into long format
 
+nice_by_day_clinical <- nice_by_day_clinical[,c("Leeftijd","Totaal","Datum")]
+nice_by_day_IC <- nice_by_day_IC[,c("Leeftijd","Totaal","Datum")]
+
 nice_by_day_clinical$Type <- "Klinisch"
 nice_by_day_IC$Type <- "IC"
 
-colnames(nice_by_day_clinical) <- c("Leeftijd","Aanwezig","Datum","Type")
-colnames(nice_by_day_IC) <- c("Leeftijd","Aanwezig","Datum","Type")
+colnames(nice_by_day_clinical) <- c("Leeftijd","Totaal","Datum","Type")
+colnames(nice_by_day_IC) <- c("Leeftijd","Totaal","Datum","Type")
 
 nice_by_day <- rbind(nice_by_day_clinical, nice_by_day_IC)
-write.csv(nice_by_day, file = "data-nice/age/leeftijdsverdeling_datum_Klinisch_IC_long.csv", row.names = F)
+nice_by_day_wide <- spread(nice_by_day, Leeftijd, Totaal)
+
+write.csv(nice_by_day_wide, file = "data-nice/age/leeftijdsverdeling_datum_Klinisch_IC_long.csv", row.names = F)
 
 add(repo, path = "*")
 commit(repo, all = T, paste0("Update NICE age-distribution in hospital ",Sys.Date()))
