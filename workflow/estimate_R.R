feb10c01c7f840443b627f95cd3290c06abd0a22
mvanzelst <j.m.vanzelst@tilburguniversity.edu>
2021-03-29 11:31:21 +0200

    Add estimation for R - use EpiEstim

---
+++ workflow/estimate_R.R
@@ -0,0 +1,87 @@
+require(EpiEstim)
+require(scales)
+require(psych)
+
+corona <- read.csv("corrections/cases_perday.csv")
+covid.incidence <- corona[,c("Date_statistics","X2021.03.28")]
+corona.breakdown <- read.csv("data-dashboards/date_statistics_mutations.csv")
+covid.incidence <- corona.breakdown[,c("Datum","DOO")]
+
+colnames(covid.incidence) <- c("dates","I")
+covid.incidence$dates <- as.Date(covid.incidence$dates)
+covid.incidence$I <- as.numeric(covid.incidence$I)
+
+plot(as.incidence(covid.incidence$I,dates = covid.incidence$dates))
+
+covid.incidence <- covid.incidence %>%
+  filter(dates > "2021-01-01")
+
+config <- make_config(list(mean_si = 4.0, std_mean_si = 0.3,
+                           min_mean_si = 3.5, max_mean_si = 4.5,
+                           std_si = 0.5, std_std_si = 0.15,
+                           min_std_si = 0.2, max_std_si = 0.5))
+
+
+res_parametric_si <- estimate_R(covid.incidence, 
+                                method="parametric_si",
+                                config = config)
+
+#plot(res_parametric_si, legend = FALSE)
+
+covid.r <- res_parametric_si$R
+
+
+dates <- as.data.frame(covid.incidence[8:(nrow(covid.incidence)),1])
+colnames(dates) <- c("dates")
+
+covid.r <- cbind(dates,covid.r)
+
+r.rivm <- fromJSON(txt = "https://data.rivm.nl/covid-19/COVID-19_reproductiegetal.json")
+r.rivm <- r.rivm[,c("Date","Rt_avg","Rt_low","Rt_up")]
+colnames(r.rivm) <- c("dates","Rt_avg","Rt_low","Rt_up")
+r.rivm$dates <- as.Date(r.rivm$dates)
+
+covid.r <- merge(covid.r, r.rivm,by="dates")
+str(covid.r)
+covid.r <- covid.r[,c("dates","Mean(R)","Std(R)","Rt_avg","Rt_low","Rt_up")]
+covid.r$R_lagged <- lead(covid.r$`Mean(R)`,6)
+covid.r$R_Std_lag <- lead(covid.r$`Std(R)`,6)
+
+
+covid.r$Rt_avg <- as.numeric(covid.r$Rt_avg)
+covid.r$R_lagged <- as.numeric(covid.r$R_lagged)
+covid.r$Std_R <- as.numeric(covid.r$`Std(R)`)
+covid.r$R_est_up <- covid.r$R_lagged+covid.r$R_Std_lag*1.96
+covid.r$R_est_low <- covid.r$R_lagged-covid.r$R_Std_lag*1.96
+
+covid.r$diff <- round(covid.r$R_lagged-covid.r$Rt_avg,2)
+
+
+covid.r %>%
+  ggplot(aes(x=dates, y=Rt_avg)) + 
+  geom_line(aes(y = Rt_avg, color = "R - RIVM"), lwd=0.8) +
+  geom_line(aes(y = R_lagged, color = "R - Marino"), lwd=1.2) +
+  #geom_line(aes(y = R_est_up, color = "R - CI (upper bound)"), lwd=1.2, linetype = "dashed") +
+  #geom_line(aes(y = R_est_low, color = "R - CI (lower bound)"), lwd=1.2, linetype = "dashed") +
+  scale_x_date(date_breaks = "1 month") +
+  scale_y_continuous(limits = c(0.75, 1.35)) +
+  scale_color_manual(values = c("#F58121","#228AC7")) +
+  theme_minimal() +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.x.bottom = element_text(size=12),
+        axis.text.y = element_text(size=12, face="bold"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+        plot.subtitle = element_text(hjust = 0.5, size = 12),
+        legend.pos = "bottom",
+        legend.direction = "vertical",
+        legend.title = element_blank()) +
+  labs(x = "Datum",
+       y = "Besmettingen per dag",
+       color = "Legend") +
+  geom_hline(yintercept = 1, linetype = "dashed") +
+  ggtitle("Reproductiegetal")
+
+
