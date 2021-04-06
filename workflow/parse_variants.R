da7aa03f2b96ba1d9c409261f40d71ba24745671
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2021-04-06 15:28:05 +0200

    Week 14 - Weekly (automated) update variants data

--- workflow/parse_variants.R
+++ workflow/parse_variants.R
@@ -3,7 +3,8 @@ webpage <- read_html(u)
 table <- as.data.frame(html_table(webpage))
 
 colnames(table) <- c("Week","Aantal_monsters","Britse_variant","ZuidAfrikaanse_variant","Braziliaanse_variant_P1",
-                     "Britse_variant_E484K","B.1.525_variant_E484K_F888L","Californie_variant","Braziliaanse_variant_P2")
+                     "Britse_variant_E484K","B.1.525_variant_E484K_F888L","Californie_variant","Braziliaanse_variant_P2",
+                     "New_York_variant","Filipijnen_variant_P3","Bretagne_variant")
 
 variants.prevalence <- table %>%
   mutate(prevalentie_britsevariant = round(Britse_variant/Aantal_monsters*100,2)) %>%
