fc303c0866d4d593b63246ef0f8d62c919495850
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2021-04-13 17:07:00 +0200

    Update coronaboete data

--- misc/scripts/coronaboetes.R
+++ misc/scripts/coronaboetes.R
@@ -16,9 +16,33 @@ df <- df %>%
   mutate(Verbod_alcohol = round(Verbod.alcohol/population*100,2)) %>%
   mutate(Mondkapje_OV = round(Mondkapjesplicht.in.personenvervoer/population*100,2)) %>%
   mutate(Avondklok = round(Avondklok/population*100,2)) %>%
-  mutate(Totaal = round(Totaal/population*100,2))
+  mutate(Totaal_percentage = round(Totaal/population*100,2))
 
 total.data <- df %>%
   select(Municipality_name:Totaal,Municipality_code:Totaal_percentage)
 
 write.csv(total.data, file = "misc/coronaboetes.csv")
+
+cases <- read.csv("data-rivm/municipal-datasets-per-day/rivm_municipality_perday_2021-03-31.csv.gz")
+cases.cumulative <- aggregate(Total_reported ~ Municipality_name, data=cases,FUN=sum)
+hosp.cumulative <- aggregate(Hospital_admission ~ Municipality_name, data=cases,FUN=sum)
+deaths.cumulative <- aggregate(Deceased ~ Municipality_name, data=cases,FUN=sum)
+
+total.data <- merge(total.data,cases.cumulative,by="Municipality_name")
+total.data <- merge(total.data,hosp.cumulative,by="Municipality_name")
+total.data <- merge(total.data,deaths.cumulative,by="Municipality_name")
+
+total.data$perc_infected <- total.data$Total_reported/total.data$population*100
+total.data$perc_hospital <- total.data$Hospital_admission/total.data$population*100
+total.data$perc_deceased <- total.data$Deceased/total.data$population*100
+
+cor(total.data$Totaal_percentage,total.data$perc_infected)
+cor(total.data$Totaal_percentage,total.data$perc_hospital)
+cor(total.data$Totaal_percentage,total.data$perc_deceased)
+
+
+hospital.nice <- read.csv("data-rivm/municipal-hospital-datasets/rivm_hospital_2021-03-31.csv.gz")
+hospital.nice.cumulative <- aggregate(Hospital_admission ~ Municipality_name, data=cases,FUN=sum)
+total.data <- merge(total.data,hospital.nice.cumulative,by="Municipality_name")
+total.data$perc_hospital_nice <- total.data$Hospital_admission.y/total.data$population*100
+cor(total.data$Totaal_percentage,total.data$perc_hospital_nice)
