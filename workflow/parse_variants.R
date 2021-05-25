e7b4c56a62c7a45a9f381b2566eae5231aa318d1
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2021-05-25 15:43:21 +0200

    Week 21 - Weekly (automated) update variants data

--- workflow/parse_variants.R
+++ workflow/parse_variants.R
@@ -14,7 +14,7 @@ table$year <- parse_number(str_sub(row.names(table),start = 1, end = 5))
 rownames(table) <- c()
 
 colnames(table) <- c("Aantal_monsters","Britse_variant","Britse_variant_E484K","ZuidAfrikaanse_variant","Braziliaanse_variant_P1",
-                     "Indiase_Variant_B1.167.1/3","Indiase_Variant_B1.167.2","B.1.525_variant_E484K_F888L","B.1.620",
+                     "Indiase_Variant_B1.167.2","Indiase_Variant_B1.167.1","Indiase_Variant_B1.167.3","B.1.525_variant_E484K_F888L","B.1.620",
                      "Colombiaanse_variant_B.1.621","Californie_variant","Filipijnen_variant_P3","Bretagne_variant",
                      "Week","Jaar")
 
@@ -31,7 +31,9 @@ mutate(prevalentie_B1.167.2 = round(Indiase_Variant_B1.167.2/Aantal_monsters*100
 
 write.csv(variants.prevalence,"data-misc/variants-rivm/prevalence_variants.csv",row.names = F)
 
-variants.old <- read.csv("data-misc/variants-rivm/prevalence_variants_archive.csv")[1:14,]
+variants.old.row <- first(variants.prevalence$Week)+4
+variants.old <- read.csv("https://raw.githubusercontent.com/mzelst/covid-19/master/data-misc/variants-rivm/prevalence_variants.csv")[1:variants.old.row,]
+
 variants.new <- read.csv("data-misc/variants-rivm/prevalence_variants.csv")
 
 variants.prevalence <- rbind(variants.old, variants.new)
