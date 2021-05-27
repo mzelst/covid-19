4b225b77258ecccd67d819bb5c901088da24a9a9
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2021-05-27 15:20:18 +0200

    [2021-05-27] Daily (automated) update RIVM and NICE data part 1/2

--- workflow/daily_update.R
+++ workflow/daily_update.R
@@ -447,4 +447,4 @@ repeat {
 
 rm(daily_vaccin_datalist,df.care.institutions,df.doctors,df.ggd,df.hospitals,df.total,
    myfiles,vaccine_data,vaccines_by_day,vaccines_delivery,dashboard.date,filename.daily.vaccins,
-   filename.daily.vaccins.delivered,last.date,temp,today.date)
+   filename.daily.vaccins.delivered,last.date,temp,today.date, cbs.filter, vaccine.data)
